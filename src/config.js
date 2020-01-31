05c35ea283775676c3efb5e96f1832dce6e94ffc
sean <seanktk@gmail.com>
2020-01-31 14:09:22 +0800

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "ap-southeast-1",
+        BUCKET: "sean-notes-app-uploads"
+    },
+    apiGateway: {
+        REGION:"ap-southeast-1",
+        URL:"https://2vqclwa4uk.execute-api.ap-southeast-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION:"ap-southeast-1",
+        USER_POOL_ID:"ap-southeast-1_2sTlmtsFS",
+        APP_CLIENT_ID:"6vvfbc0o9iked5h7vh3aiqlfa8",
+        IDENTITY_POOL_ID:"ap-southeast-1:4ab6bdd9-e2af-4186-8598-f8d691b2b30e"
+    }
+}
